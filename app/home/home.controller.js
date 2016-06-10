4623a5d13b71c2652442d5b0bceff821bf128539
almoanna <anna.duda181@gmail.com>
2016-06-10 19:46:27 +0000

    nadawanie statusow

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -2,15 +2,17 @@ export default class HomeController{
     constructor($scope,$stateParams)
     {
       this.todos = [];
-      this.user = $stateParams.user
-      this.userText = ''; 
+      this.user = $stateParams.user;
     }
     addTodo()
     {
         this.todos.push({
-            user: this.user,
-            title: this.userText,
-            completed:false
+            value:{
+              user: this.user,
+              title: this.userText,
+              completed:false
+           },
+            editing: false
         });
     }
     
@@ -29,4 +31,28 @@ export default class HomeController{
         var index = this.todos.indexOf(toDo);
         this.todos.splice(index,1);
      }
+     editTodo(todo)
+    {
+        todo.editing = !todo.editing;
+        this.originalToDo = todo;
+    }
+    
+    doneEditing(todo)
+    {
+        todo.editing = false;
+        console.log(todo.editing);
+    }
+    onChangeStatus(status)
+    {
+        this.statusFilter = (status === 'active') ?
+				{ value:{completed: false }} : (status === 'completed') ?
+				{ value:{completed: true }} : {};
+				 /*     if(status==='active')
+        return completed: false;
+        else if(status==='completed')
+           return completed: true;
+        else return {};*/
+    }
 }
+
+
