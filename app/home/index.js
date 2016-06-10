ebe8bdb3dd9459e1f36a1f39de416d3c2360adae
almoanna <anna.duda181@gmail.com>
2016-06-10 19:03:33 +0000

    srech view

--- app/home/index.js
+++ app/home/index.js
@@ -3,8 +3,10 @@ import uirouter from 'angular-ui-router';
 import routing from './home.routes';
 
 import homeController from './home.controller';
+import userInput from './directives/user_input';
 
 export default angular.module('app.home',[uirouter])
     .config(routing)
     .controller('homeController',homeController)
+    .directive('userInput', userInput)
     .name; 
