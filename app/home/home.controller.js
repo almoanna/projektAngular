2bd75a24b2ac83e80130a4d5edb31dc8fd5d9531
almoanna <anna.duda181@gmail.com>
2016-06-10 19:33:08 +0000

    dodawanie i usuwanie

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,6 +1,32 @@
 export default class HomeController{
-    constructor($stateParams)
+    constructor($scope,$stateParams)
     {
-     this.user = $stateParams.user;   
+      this.todos = [];
+      this.user = $stateParams.user
+      this.userText = ''; 
     }
+    addTodo()
+    {
+        this.todos.push({
+            user: this.user,
+            title: this.userText,
+            completed:false
+        });
+    }
+    
+    onuser()
+    {
+        this.selectedTab = 'user';
+    }
+    
+    onall()
+    {
+        this.selectedTab = 'all';
+    }
+    
+    removeTodo(toDo)
+    {
+        var index = this.todos.indexOf(toDo);
+        this.todos.splice(index,1);
+     }
 }
